916b246eaee364e6d7801de3aa5eab40cfd85d67
marufmahin <maruf907182@gmail.com>
2025-11-07 19:27:29 +0600

    Copy button functionality added

--- script.js
+++ script.js
@@ -15,3 +15,27 @@ for (let heart of hearts) {
   });
 }
 
+//Copy Button
+const copyButtons = document.querySelectorAll(".copy-btn");
+
+for (const btn of copyButtons) {
+    btn.addEventListener('click', function() {
+        
+        const number = btn.parentNode.parentNode.children[2].querySelector("h2").innerText;
+        navigator.clipboard.writeText(number);
+
+       
+        let totalCopyDesktop = document.getElementById('copy-count-desktop');
+        let crrCopyDesktop = Number(totalCopyDesktop.innerText);
+        crrCopyDesktop++;
+        totalCopyDesktop.innerText = crrCopyDesktop;
+
+        let totalCopyMobile = document.getElementById('copy-count-mobile');
+        let crrCopyMobile = Number(totalCopyMobile.innerText);
+        crrCopyMobile++;
+        totalCopyMobile.innerText = crrCopyMobile;
+
+        alert("Number copied: "+ number);
+    });
+}
+
